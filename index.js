c149d50a2f30836be24da4d5060fa422810cbc2f
Ron Artenstein <ronartenstein@gmail.com>
2021-06-10 14:29:57 +0300

    NPM modules and element for guide postition

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/pagetwo.html");
 });
 app.listen(3000, function () {
   console.log("Server is running on localhost3000");
 });
+if (true) {
+  console.log("this is true");
+} else {
+  console.log("false");
+}
